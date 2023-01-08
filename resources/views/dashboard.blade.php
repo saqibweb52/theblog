972f7f7ca9cbd3128d75667373e8ffc17755aa10
saqib <saqibweb52@gmail.com>
2023-01-08 15:32:59 +0500

    both athrizd & unathrizd can't see a block of code.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,7 +28,7 @@
                 <td class="border px-6 py-4">{{$p->body}}</td>
                 <td class="border px-6 py-4">{{$p->created_at}}</td>
                 <td class="border px-6 py-4">
-                    @can('isAdmin', $posts)
+                    @can('show', $posts)
                 <a href="{{url('editpost',$p->id)}}"><i class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Edit</i></a>
                 <a href="{{url('deletepost',$p->id)}}"><i class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">Delete</i></a>
                 @endcan
